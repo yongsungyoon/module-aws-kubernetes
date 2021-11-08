80136f7bf66d46104a35a5e1f5c50656c6a7027e
윤용성(Yongsung Yoon)/개발혁신담당/11ST <yongsung.yoon@sk.com>
2021-11-08 10:01:32 +0900

    Fix policy json problem - leading spaces

--- main.tf
+++ main.tf
@@ -10,19 +10,19 @@ locals {
 resource "aws_iam_role" "ms-cluster" {
   name               = local.cluster_name
   assume_role_policy = <<POLICY
-  {
-    "Version": "2012-10-17",
-    "Statement" : [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "eks.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-    ]
-  }
-  POLICY
+{
+  "Version": "2012-10-17",
+  "Statement" : [
+      {
+          "Effect": "Allow",
+          "Principal": {
+              "Service": "eks.amazonaws.com"
+          },
+          "Action": "sts:AssumeRole"
+      }
+  ]
+}
+POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "ms-cluster-AmazonEKSClusterPolicy" {
